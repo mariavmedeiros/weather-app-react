e36e720282160cea094e49d6a2f6b8b29b39a439
Maria Medeiros <maitevelori@hotmail.com>
2022-06-19 15:46:04 -0700

    deleted app.css changed the title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App</div>;
+  return (
+    <div className="App">
+      {" "}
+      <h1>Weather App</h1>
+      <footer>
+        <a href="https://github.com/mariavmedeiros/weather-app-react.git">
+          Open-source code
+        </a>{" "}
+        by Maria Medeiros
+      </footer>
+    </div>
+  );
 }
