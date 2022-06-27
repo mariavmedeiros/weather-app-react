bcb6fb9dbcf713c81385e81c8f2f71b816c54298
Maria Medeiros <maitevelori@hotmail.com>
2022-06-27 15:50:48 -0700

    added daily forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,9 +15,15 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col">
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
