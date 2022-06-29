1827d18a37ab304e9027d4c5baaf5031d9db3d70
Maria Medeiros <maitevelori@hotmail.com>
2022-06-28 17:09:46 -0700

    fixed the days of the week

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -14,7 +14,7 @@ export default function WeatherForecastDay(props) {
   function day() {
     let date = new Date(props.data.dt * 1000);
     let day = date.getDay();
-    let days = ["Sun", "Mon", "Tue", "Thu", "Fri", "Sat"];
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
     return days[day];
   }
